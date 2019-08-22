ac857c750ea137e050ac86eabc9e41f6c8794bdd
Michele "Ubik" De Simoni <ubik@ubik.tech>
2019-08-22 20:49:31 +0200

    Site core ready

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "sanitize.css"
 import "sanitize.css/forms.css"
 import "sanitize.css/typography.css"
+import "@fortawesome/fontawesome-free/css/all.css"
 
-import "./style/base.css"
-import "./style/customGrid.css"
+import "./style/base.scss"
